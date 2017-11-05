ad4092d53a13a77e5bfc523c4a49567e3d4dc4d5
RoshanADK <roshannadhikari039@gmail.com>
2017-11-05 12:52:33 +0530

    Montecarlo Method

---
+++ TF_MontecarloMethod.py
@@ -0,0 +1,11 @@
+
+
+# Montecarlo's method is widely used in the application of high performance scientific computing
+
+import tensorflow as tf 
+
+trials = 100
+hits = 0
+
+# Let's generate pseudorandom points 
+
